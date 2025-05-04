f6bf0b874921a6606b52d6374a4338f1fbbfa4f2
Dumitru Vlad-Mihai <vlad.liviu22@gmail.com>
2025-05-05 00:12:45 +0300

    highscore added

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -14,14 +15,16 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
     
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def update_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
         
